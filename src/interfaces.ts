3012d65d42e16e9c0f97f1e59b3802286156ac60
cdaringe <cdaringe@gmail.com>
2019-12-01 13:37:41 -0800

    feat: init

---
+++ src/interfaces.ts
@@ -0,0 +1,24 @@
+// export type CreateScene = {
+//   bindUndo?: (undo: () => void) => void
+//   canEditPoint?: (state: State, point: Point, pointIndex: number) => boolean
+//   onStateChange?: (state: State) => void
+//   points: Point[]
+//   svg$: D3SVG
+// }
+
+export type D3Circle = d3.Selection<SVGCircleElement, any, any, any>
+export type D3Path = d3.Selection<SVGPathElement, any, any, any>
+export type D3Selection = d3.Selection<any, any, any, any>
+export type D3SVG = d3.Selection<SVGSVGElement, unknown, null, undefined>
+export type Point = [number, number]
+
+export type MetaNode = {
+  isDirty?: boolean
+  node?: {
+    dragStartPoint?: Point
+    node$: D3Circle
+  }
+  point: Point
+}
+
+export type NodeMove = { x: number; y: number }
