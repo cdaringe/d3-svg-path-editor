49b5e2786dbd8ee7c5d0aac3e781c28085664f7c
cdaringe <cdaringe@gmail.com>
2019-12-01 14:23:37 -0800

    Updates

--- util/handle-key-events.ts
+++
@@ -1,7 +0,0 @@
-import { isUndo } from './undo-redo'
-
-export const bindKeyEvents = ({ onUndo }: { onUndo: () => void }) => {
-  window.onkeydown = (evt: KeyboardEvent) => {
-    if (isUndo(evt, 'mac')) onUndo()
-  }
-}
