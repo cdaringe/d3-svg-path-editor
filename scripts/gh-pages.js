f87092a71d9fe7e0dc784c50efbdc06b63bc2718
cdaringe <cdaringe@gmail.com>
2019-12-05 22:29:53 -0800

    chore: improve doc publishing script

--- scripts/gh-pages.js
+++ scripts/gh-pages.js
@@ -1,11 +1,19 @@
+require('perish')
 var pages = require('gh-pages')
 var { resolve } = require('path')
 var distDirname = resolve(__dirname, '..', 'dist')
 
-pages.publish(distDirname, {
-  user: {
-    name: 'cdaringe',
-    email: 'cdaringe@cdaringe.com',
-    silent: true
+pages.publish(
+  distDirname,
+  {
+    user: {
+      name: 'cdaringe',
+      email: 'cdaringe@cdaringe.com',
+      silent: true
+    }
+  },
+  (err, res) => {
+    if (err) throw err
+    console.log({ res })
   }
-})
+)
